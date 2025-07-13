64bdad2240aee69232fa667e2dd4c3823c765b70
cenesdeveloper <cenesdurak@gmail.com>
2025-07-13 19:33:39 -0400

    Drizzle ORM

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -4,10 +4,14 @@ import { z } from 'zod';
 
 import { getUser } from '../kinde';
 
+import { db } from '../db';
+import { expenses as expenseTable } from '../db/schema/expenses';
+import { eq } from 'drizzle-orm';
+
 const expenseSchema = z.object({
     id: z.number().int().positive().min(1),
     title: z.string().min(3).max(100),
-    amount: z.number().int().positive()
+    amount: z.string()
 });
 
 type Expense = z.infer<typeof expenseSchema>;
@@ -15,27 +19,39 @@ type Expense = z.infer<typeof expenseSchema>;
 const createPostSchema = expenseSchema.omit({ id: true });
 
 const fakeExpenses: Expense[] = [
-    {id: 1, title: "Groceries", amount: 50},
-    {id: 2, title: "Utilities", amount: 100},
-    {id: 3, title: "Rent", amount: 1000}
+    {id: 1, title: "Groceries", amount: "50"},
+    {id: 2, title: "Utilities", amount: "100"},
+    {id: 3, title: "Rent", amount: "1000"}
 ]
 
 
 export const expensesRoute = new Hono()
-.get("/", getUser, (c) => {
+.get("/", getUser, async (c) => {
     const user = c.var.user;
-    return c.json({ expenses: fakeExpenses })
+
+    const expenses = await db
+    .select()
+    .from(expenseTable)
+    .where(eq(expenseTable.userId, user.id));
+
+    return c.json({ expenses: expenses });
 })
 .post("/", getUser, zValidator("json", createPostSchema), async (c) => {
-    const data = await c.req.valid("json")
-    const expense = createPostSchema.parse(data)
-    fakeExpenses.push({...expense, id: fakeExpenses.length+1})
+    const expense = await c.req.valid("json");
+    const user = c.var.user;
+    const result = await db.insert(expenseTable).values({
+        ...expense,
+        userId: user.id,
+        date: new Date().toISOString() // or provide the appropriate date value
+    }).returning();
+    
+
     c.status(201);
     return c.json(expense);
 })
 .get("/total-spent", getUser, async (c) => {
     await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate delay
-    const total = fakeExpenses.reduce((acc, expense) => acc + expense.amount, 0);
+    const total = fakeExpenses.reduce((acc, expense) => acc + +expense.amount, 0);
     return c.json({ total });
 })
 .get("/:id{[0-9]+}", getUser, c => {
