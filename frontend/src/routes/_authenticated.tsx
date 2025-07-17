efd87bb3b92c4ffd6e87b72bfb3dca32f55658e8
cenesdeveloper <cenesdurak@gmail.com>
2025-07-17 17:40:43 -0400

    UI update

--- frontend/src/routes/_authenticated.tsx
+++ frontend/src/routes/_authenticated.tsx
@@ -4,9 +4,14 @@ import { Button } from "@/components/ui/button";
 
 const Login = () => {
   return (
-    <div>
-      You have to login
-      <a href="/api/login">Login!</a>
+    <div className="flex flex-col gap-y-2 items-center">
+      <p className="text-xl font-bold">Please log in or register</p>
+      <Button asChild className="mt-4">
+        <a href="/api/login">Login</a>
+      </Button>
+      <Button asChild className="mt-4">
+        <a href="/api/register">Register</a>
+      </Button>
     </div>
   );
 };
--- frontend/src/routes/_authenticated/profile.tsx
+++ frontend/src/routes/_authenticated/profile.tsx
@@ -2,6 +2,14 @@ import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
 import { userQueryOptions } from '@/lib/api'
 
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+
+import { Button } from '@/components/ui/button' 
+
 
 export const Route = createFileRoute('/_authenticated/profile')({
   component: Profile,
@@ -15,8 +23,19 @@ function Profile() {
   if (error) return 'not logged in';
 
   return <div className="p-2">
-    Hello from Profile!
-    <p>Hello {data.user.family_name}</p>
-    <a href="/api/logout">Logout!</a>
+    <div className="flex items-center gap-2">
+        <Avatar>
+          {data.user.picture && (
+            <AvatarImage
+              src={data.user.picture}
+              alt={data.user.given_name}
+            />)}
+            <AvatarFallback>{data.user.given_name}</AvatarFallback>
+          </Avatar>
+    
+    <p>{data.user.given_name} {data.user.family_name}</p>
+    </div>
+    <Button asChild className='my-4'><a href="/api/logout">Logout!</a></Button>
+    
   </div>
 }
